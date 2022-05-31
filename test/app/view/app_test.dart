59394d08ea4e544edf4ca3e40ac3843fc54a4ae3
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-05-31 09:33:45 +0200

    Updated app test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,28 @@
 import 'package:doggos/app/app.dart';
 import 'package:doggos/home/home.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   group('App', () {
+    late HomeBloc homeBloc;
+    setUp(() {
+      final client = http.Client();
+      homeBloc = HomeBloc(
+        dogsRepository: DogsRepository(
+          dogsProvider: DogsProvider(client: client),
+        ),
+      );
+    });
+
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
+      await tester.pumpWidget(
+        BlocProvider.value(
+          value: homeBloc,
+          child: const App(),
+        ),
+      );
       expect(find.byType(HomePage), findsOneWidget);
     });
   });
